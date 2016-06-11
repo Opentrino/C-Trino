6261b8f6005045840f7627c7db595af8cc2648be
miguelangelo78 <santosmiguel25@gmail.com>
2016-06-11 12:19:23 +0100

    Scripts for Debian (Linux Mint) added

--- src/core_main.sv
+++ src/core_main.sv
@@ -1,66 +1,48 @@
-module arbiter (
-clock, 
-reset, 
-req_0,
-req_1, 
-gnt_0,
-gnt_1
-);
-
-input clock, reset, req_0, req_1;
-output gnt_0, gnt_1;
-
-reg gnt_0, gnt_1;
-
-always @ (posedge clock or posedge reset)
-if (reset) begin
- gnt_0 <= 0;
- gnt_1 <= 0;
-end else if (req_0) begin
-  gnt_0 <= 1;
-  gnt_1 <= 0;
-end else if (req_1) begin
-  gnt_0 <= 0;
-  gnt_1 <= 1;
-end
-
+module dff(d,clk,reset,q,qbar);
+	input wire d, reset, clk;
+	output reg q, qbar;
+		
+	always@(posedge clk) begin
+		q <= d;
+		qbar <= !d;
+	end
+	
+	always@(posedge reset) begin
+		q <= 0;
+		qbar <= 1;	
+	end
 endmodule
-// Testbench Code Goes here
-module arbiter_tb;
 
-reg clock, reset, req0,req1;
-wire gnt0,gnt1;
+module dff_t;
+	reg d,clk,reset;
+	wire q, qbar;
+	dff dflipflop(d,clk,reset, q,qbar);
+	
+	task set; 
+		input new_d; 
+	begin
+		d = new_d;
+		clk = 0;
+		#1;
+		clk = 1;
+		#1;
+		clk = 0;
+	end
+	endtask 
 
-initial begin
-  $monitor ("req0=%b,req1=%b,gnt0=%b,gnt1=%b", req0,req1,gnt0,gnt1);
-  $dumpfile("core_main.vcd");
-  $dumpvars(reset);
-  clock = 0;
-  reset = 0;
-  req0 = 0;
-  req1 = 0;
-  #5 reset = 1;
-  #15 reset = 0;
-  #10 req0 = 1;
-  #10 req0 = 0;
-  #10 req1 = 1;
-  #10 req1 = 0;
-  #10 {req0,req1} = 2'b11;
-  #10 {req0,req1} = 2'b00;
-  #10 $finish;
-end
-
-always begin
- #5 clock = !clock;
-end
-
-arbiter U0 (
-.clock (clock),
-.reset (reset),
-.req_0 (req0),
-.req_1 (req1),
-.gnt_0 (gnt0),
-.gnt_1 (gnt1)
-);
-
+	task init;
+	begin
+		clk = 0;
+		reset = 1;
+		#1;
+		reset = 0;
+	end endtask
+	initial begin
+		$monitor("d: %d clk: %d q: %d ~q: %d", d,clk,q,qbar);
+		$dumpfile("core_main.vcd");
+		$dumpvars(d,reset,clk,q,qbar);
+		init();
+		set(1);
+		set(1);
+	end
 endmodule
